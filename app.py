ccae695a290a29817eb31b31a9fccc5a6d50b4e9
Shail Pradhan <[shailprd69@gmail.com]>
2025-04-18 12:05:00 +0530

    1

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import numpy as np
 from PIL import Image
 import csv
 import mediapipe as mp
+import os
 
 from model import KeyPointClassifier
 
@@ -85,4 +86,5 @@ def predict():
 
 # === Entry Point ===
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
